5c30d044993796e41c6171a4c34740e554f0e9dd
Renata Varga <reni_varga@outlook.com>
2024-07-27 12:51:26 +0300

    Add GitHub Actions workflow configuration

---
+++ test/test2.js
@@ -0,0 +1,8 @@
+// test/test2.js
+const assert = require('assert');
+
+describe('String', function() {
+  it('should return the correct length', function() {
+    assert.equal('hello'.length, 5);
+  });
+});
